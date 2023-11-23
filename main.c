b4728068db0625b8b11d419dc12b164db1058754
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-11-23 16:17:32 -0500

    2 commit

--- main.c
+++ main.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 int main(){
-
-
+    int filas, columnas;
+    printf("Ingrese el número de filas: ");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas: ");
+    scanf("%d", &columnas);
+    int matriz[filas][columnas];
+    srand(time(NULL));
 
 }
