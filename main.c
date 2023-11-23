5510eb7e4159af2a604439b92d24ef63b8cff6c4
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-11-23 16:19:12 -0500

    3 commit

--- main.c
+++ main.c
@@ -9,5 +9,18 @@ int main(){
     scanf("%d", &columnas);
     int matriz[filas][columnas];
     srand(time(NULL));
-
+    // aqui se llena los numeros aleatorios de 0 a 100
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[i][j] = rand() % 101;
+        }
+    }
+    // aqui se imprime la matriz original
+    printf("Matriz original:\n");
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
 }
